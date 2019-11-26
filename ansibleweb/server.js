b56c59808bd3407ac43582d34db8260f9f2a16d6
leooni <53792010+leooni@users.noreply.github.com>
2019-11-26 10:19:25 -0300

    Update server.js

--- ansibleweb/server.js
+++ ansibleweb/server.js
@@ -13,64 +13,66 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-
-//var pkginfo = require('pkginfo')(module);
 var pkginfo = require('pkginfo')(module);
 var pkg = module.exports;
 app.use(bodyParser.json());
 
 app.post('/add', function (req, res) {
     const add = req.body;
-    //console.log("Sucesso");
+
+       const options = {
+            hostname: 'student1.08b6.rhdemo.io',
+            port: 443,
+            path:'/api/v2/projects/4/',
+            method: 'POST',
+            headers: {     // autenticação via Bearer Key gerado no Tower
+                'Content-Type': 'application/octet-stream',
+                'Authorization': 'Bearer 9KDMp2a3yeWvNQLhYvunCZKD9Iv5d9'
+            },
+            encoding: null, //  if you expect binary data
+            responseType: 'buffer',
+            body: new Uint8Array(3)
+        };
     res.send(add);
     console.log(add);
 })
 
-
 router.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname + '/about.html'));
+    res.sendFile(path.join(__dirname + '/about.html'));
 });
 
 router.get('/sitemap', function (req, res) {
-  res.sendFile(path.join(__dirname + '/sitemap.html'));
+    res.sendFile(path.join(__dirname + '/sitemap.html'));
 });
 
-
-router.get('/', function (req, res) {
-  res.render('index', {'pkginfo' : pkg});
-
-
-const options = {
-  hostname: 'student3.4947.rhdemo.io',
-  port: 443,
-  // path: '/api/o/',
-  path:'/api/v2/job_templates/',
-  authorize:'admin:G1z1e7MC4TpM9Z',
-  method: 'POST'
-};
+router.get('/', function (req, res,) {
+    res.render('index', {'pkginfo' : pkg});
 
 
-const request = https.request(options, (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
+    const request = https.request(options, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
 
-  res.on('data', (d) => {
-    process.stdout.write();
-  });
-});
+         res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+         });
+         res.on('end', () => {
+             console.log('No more data in response.');
 
-request.on('error', (e) => {
-  console.error(e);
-});
-request.end();
+         });
+
+    });
+
+    request.on('error', (e) => {
+        console.error(e);
+    });
+    request.end();
 
 });
-
-
-
 
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(process.env.port || 3003);
 
-console.log('Running at Port 3000');
+console.log('Running at Port 3003');
+
